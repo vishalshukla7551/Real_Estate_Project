7537ca295c79fefa10e64a64e820a9f73857b250
Vishal Shukla <vishalshukla1029@gmail.com>
2025-04-04 23:48:28 +0530

    testing

--- api/app.js
+++ api/app.js
@@ -78,7 +78,7 @@ app.use("/api/messages", messageRoute);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
-
+console.log("server running")
 // const PORT = process.env.PORT || 8800;
 // app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
 export default app;
